a60df19870e9b83644d5a2a5a4f7e6a660a6a503
Electron7-7 <julian@frattarola.net>
2025-07-31 14:09:35 -0400

    resources(added new font)

--- src/embedded/fonts.h
+++ src/embedded/fonts.h
@@ -1,6 +1,9 @@
 #ifndef FONTS_H
 #define FONTS_H
 
+extern unsigned char font_DejaVuSansMono_ttf[];
+extern unsigned int  font_DejaVuSansMono_ttf_len;
+
 extern unsigned char font_Verdana_ttf[];
 extern unsigned int  font_Verdana_ttf_len;
 
