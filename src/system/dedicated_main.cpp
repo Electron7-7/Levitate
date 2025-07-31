879db4fe02c643051047e62990c7e5f69c8e8b5d
Electron7-7 <julian@frattarola.net>
2025-07-31 14:08:23 -0400

    feature(new, slimmer main file)

--- src/system/dedicated_main.cpp
+++ src/system/dedicated_main.cpp
@@ -1,100 +1,109 @@
-#define GLM_ENABLE_EXPERIMENTAL
-#include "glad/glad.h"
+#include "editor/lines.hpp"
 #include "common/labels.hpp"
-#include "text_engine/l_input.hpp"
-#include "gui/rendering/l_rendering.hpp"
-#include "gui/e_scape.hpp"
+#include "freetype/freetype.h"
+#include "rendering/gl_shader.hpp"
+#include "rendering/main_window.hpp"
+#include "rendering/prototype_renderer.hpp"
 #include "embedded/glsl.hpp"
+#include "embedded/fonts.h"
 
-#include <curses.h>
+#include <glad/glad.h>
 #include <GLFW/glfw3.h>
-#include <glm/fwd.hpp>
-#include <glm/glm.hpp>
-#include <glm/gtx/string_cast.hpp>
 
-glm::vec2 main_window_size(1280.0f, 720.0f);
-
-void mouseCallback(GLFWwindow *window, double x_position_in, double y_position_in);
 void keyCallback(GLFWwindow *window, int key, int scancode, int action, int mods);
-void frameBufferSizeCallback(GLFWwindow* window, int width, int height);
 void characterCallback(GLFWwindow* window, unsigned int codepoint);
 
-int main()
+int main(int argc, char** argv)
 {
-	glfwInit();
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
-	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
-	GLFWwindow *main_window = glfwCreateWindow(main_window_size[0], main_window_size[1], "Levitate", nullptr, nullptr);
-	glfwMakeContextCurrent(main_window);
+    GLFWwindow *main_window = glfwCreateWindow(MainWindow::Width, MainWindow::Height, "Levitate", nullptr, nullptr);
+    glfwMakeContextCurrent(main_window);
 
-	if(main_window == nullptr)
-	{
-		printf("%s Failed to create GLFW window!%s\n", ERROR, COLOR_RESET);
-		glfwTerminate();
-	}
+    if(main_window == nullptr)
+    {
+        printf("%s Failed to create GLFW window!%s\n", ERROR, COLOR_RESET);
+        glfwTerminate();
+    }
 
-	if(!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
-		printf("%s Failed to initialize GLAD!%s\n", ERROR, COLOR_RESET);
+    if(!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+        printf("%s Failed to initialize GLAD!%s\n", ERROR, COLOR_RESET);
 
-	const GLFWvidmode *primary_monitor_video_mode = glfwGetVideoMode(glfwGetPrimaryMonitor());
-	int primary_monitor_xposition = 0;
-	int primary_monitor_yposition = 0;
-	glfwGetMonitorPos(glfwGetPrimaryMonitor(), &primary_monitor_xposition, &primary_monitor_yposition);
-	glfwSetWindowPos(main_window, static_cast<int>(((primary_monitor_video_mode->width - main_window_size[0]) / 2) + primary_monitor_xposition), static_cast<int>(((primary_monitor_video_mode->height - main_window_size[1]) / 2) + primary_monitor_yposition));
-	glfwSetInputMode(main_window, GLFW_CURSOR, GLFW_CURSOR_NORMAL);
-	glfwSetFramebufferSizeCallback(main_window, frameBufferSizeCallback);
-	glfwSetCursorPosCallback(main_window, mouseCallback);
-	glfwSetCharCallback(main_window, characterCallback);
-	glfwSetKeyCallback(main_window, keyCallback);
-	glEnable(GL_DEPTH_TEST);
-	glEnable(GL_DEBUG_OUTPUT);
+    const GLFWvidmode *primary_monitor_video_mode = glfwGetVideoMode(glfwGetPrimaryMonitor());
+    int primary_monitor_xposition = 0;
+    int primary_monitor_yposition = 0;
+    glfwGetMonitorPos(glfwGetPrimaryMonitor(), &primary_monitor_xposition, &primary_monitor_yposition);
+    glfwSetWindowPos(main_window, static_cast<int>(((primary_monitor_video_mode->width - MainWindow::Width) / 2) + primary_monitor_xposition), static_cast<int>(((primary_monitor_video_mode->height - MainWindow::Height) / 2) + primary_monitor_yposition));
+    glfwSetInputMode(main_window, GLFW_CURSOR, GLFW_CURSOR_NORMAL);
+    glfwSetKeyCallback(main_window, keyCallback);
+    glfwSetCharCallback(main_window, characterCallback);
+    glEnable(GL_DEPTH_TEST);
+    glEnable(GL_DEBUG_OUTPUT);
 
-	GLShader temp_shader(glsl_font_vert, glsl_font_frag);
-	temp_shader_pointer = &temp_shader;
+    GLShader temp_shader(glsl_font_vert, glsl_font_frag);
+    temp_shader_pointer = &temp_shader;
 
-	glGenVertexArrays(1, &TEMPORARY_VAO);
+    glGenVertexArrays(1, &TEMPORARY_VAO);
 
-	QuickShittySetupFreetype();
+    if(FT_Init_FreeType(global_freetype))
+        printf("%s FreeType library failed to initialize!%s\n", ERROR, COLOR_RESET);
 
-	while(!glfwWindowShouldClose(main_window))
-	{
-		glClearColor(0.85f, 0.8f, 0.95f, 1.0f);
-		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-		QuickShittyPrintToScreen(100.0f, main_window_size.y / 2);
-		glfwSwapBuffers(main_window);
-		glfwPollEvents();
-	}
+    QuickShittySetupFont("Verdana", font_Verdana_ttf, font_Verdana_ttf_len);
+    QuickShittySetupFont("DejaVuSansMono", font_DejaVuSansMono_ttf, font_DejaVuSansMono_ttf_len);
 
-	glfwTerminate();
-	return 0;
+    InitLines();
+
+    while(!glfwWindowShouldClose(main_window))
+    {
+        glClearColor(0.85f, 0.8f, 0.95f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+        QuickShittyPrintToScreen(0.0f, MainWindow::Height - 50.0f, 1, glm::vec3(1.0f), "DejaVuSansMono");
+        glfwSwapBuffers(main_window);
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
+    return 0;
 }
 
-void frameBufferSizeCallback(GLFWwindow *window, int width, int height)
+void keyCallback(GLFWwindow* window, int key, int scancode, int action, int mods)
 {
-	main_window_size = glm::vec2(width, height);
-	glViewport(0, 0, width, height);
-}
+    if(action != GLFW_PRESS && GLFW_REPEAT)
+        return;
 
-void mouseCallback(GLFWwindow *window, double x_position_in, double y_position_in)
-{}
+    switch(key)
+    {
+    case GLFW_KEY_ESCAPE:
+        glfwSetWindowShouldClose(window, true);
+        return;
 
-void keyCallback(GLFWwindow *window, int key, int scancode, int action, int mods)
-{
-	if(key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
-		glfwSetWindowShouldClose(window, true);
-	if((key == GLFW_KEY_BACKSPACE || key == GLFW_KEY_DELETE) && (action == GLFW_PRESS || action == GLFW_REPEAT))
-		DeleteCharacter();
-	if(key == GLFW_KEY_ENTER && (action == GLFW_PRESS || action == GLFW_REPEAT))
-		InsertNewLine();
-	if(key == GLFW_KEY_RIGHT && (action == GLFW_PRESS || action == GLFW_REPEAT))
-		moveCursorHorizontally(1);
-	if(key == GLFW_KEY_LEFT && (action == GLFW_PRESS || action == GLFW_REPEAT))
-		moveCursorHorizontally(-1);
+    case GLFW_KEY_BACKSPACE:
+        DeleteCharacters();
+        return;
+
+    case GLFW_KEY_ENTER:
+    case GLFW_KEY_DOWN:
+        MoveCaret(0, 1);
+        return;
+
+    case GLFW_KEY_UP:
+        MoveCaret(0, -1);
+        return;
+
+    case GLFW_KEY_RIGHT:
+        MoveCaret(1);
+        return;
+
+    case GLFW_KEY_LEFT:
+        MoveCaret(-1);
+        return;
+    }
 }
 
 void characterCallback(GLFWwindow* window, unsigned int codepoint)
 {
-	InsertCharacter(codepoint);
+    InsertCharacter(codepoint);
 }
