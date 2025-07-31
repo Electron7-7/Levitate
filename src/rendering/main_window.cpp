539c97ea94643e321376442a49f1a8abf37f83e4
Electron7-7 <julian@frattarola.net>
2025-07-31 14:08:09 -0400

    feature(created a struct with static variables for namespaced main window variables)

---
+++ src/rendering/main_window.cpp
@@ -0,0 +1,4 @@
+#include "main_window.hpp"
+
+float MainWindow::Width  = 800.0f;
+float MainWindow::Height = 600.0f;
---
+++ src/rendering/main_window.hpp
@@ -0,0 +1,11 @@
+#ifndef MAIN_WINDOW_H
+#define MAIN_WINDOW_H
+
+struct MainWindow
+{
+public:
+    static float Width;
+    static float Height;
+};
+
+#endif // MAIN_WINDOW_H
