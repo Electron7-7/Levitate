9d95e6658be6e46ff5769e6268feffedd076089e
Electron7-7 <26826460+Electron7-7@users.noreply.github.com>
2026-05-26 17:21:41 -0400

    remove old code

--- src/embedded/glsl.hpp
+++
@@ -1,7 +0,0 @@
-#ifndef GLSL_H
-#define GLSL_H
-
-extern const char* glsl_font_vert;
-extern const char* glsl_font_frag;
-
-#endif // GLSL_H
--- src/rendering/main_window.cpp
+++
@@ -1,4 +0,0 @@
-#include "main_window.hpp"
-
-float MainWindow::Width  = 800.0f;
-float MainWindow::Height = 600.0f;
